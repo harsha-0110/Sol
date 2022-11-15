11435d7cd9d21a32db82eabe4be21d8c2b9311d1
harsha-0110 <112392323+harsha-0110@users.noreply.github.com>
2022-11-15 13:58:46 +0530

    Update file.sol

--- file.sol
+++ file.sol
@@ -339,3 +339,30 @@ contract Greater_value{
     }
 }
 _______________________________________________________________________________________
+//Coin.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+contract Coin {
+
+    address private minter; 
+    mapping (address => uint) public balances;
+    
+    event Sent (address from, address to, uint amount);
+
+    constructor() {
+        minter = msg.sender;
+    }
+    
+    function mint (address receiver, uint amount) public {
+        require(msg.sender == minter); 
+        require(amount <1e60); 
+        balances[receiver] += amount;
+    }
+
+    function send(address receiver, uint amount) public {
+        require(amount <= balances[msg.sender], "Insufficient balance."); 
+        balances[msg.sender] -= amount; 
+        balances[receiver] += amount; 
+        emit Sent(msg.sender, receiver, amount);
+    }
+}
